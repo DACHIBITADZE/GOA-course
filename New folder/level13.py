c6f475a91e4dfaad164503cd62c2ca6e34d71b1b
DACHIBITADZE <bitadzedachi7@gmail.com>
2024-09-10 16:26:17 +0400

    commit

---
+++ New folder/level13.py
@@ -0,0 +1,16 @@
+#1. for loop-ის მეშვეობით გამოიტანეთ რიცხვი 10, 7-ჯერ.
+for x in range(7):
+    print('10')
+
+#2. for loop-ის მეშვეობით გამოიტანეთ 'hello' 8-ჯერ.
+for x in range(8):
+    print('hello')
+#3. Input()-ის გამოყენებით მომხმარებელს შემოატანინეთ მისი სახელი, შემდეგ for loop-ით კი დაპრინტეთ ("hello" + მომხმარებლის სახელი) 5 ჯერ.
+input ('user')
+for i in range(5):
+   print('helo''user')
+#4. while loop-ის მეშვეობით გამოიტანეთ ციფრები 10-დან 30-მდე.
+age = 10
+while age > 30 :
+  print('minor')
+  
