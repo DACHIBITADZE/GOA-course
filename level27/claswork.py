9ed370d9e21c58dc5e29067a09c76a369b23e288
DACHIBITADZE <bitadzedachi7@gmail.com>
2024-12-10 20:31:01 +0400

    commit

---
+++ level27/claswork.py
@@ -0,0 +1,11 @@
+secret_word = 'revazi'
+print('secret word: ******')
+print()
+  
+user_gues = input('guess a later: ') 
+
+
+print('wrong guess')
+if secret_word.find(user_gues) :
+# border: 150px;px solid grey; width: 500px; border-top-right-radius: 70%:
+  print
